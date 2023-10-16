924ac09e5582a2e4d8e851b5c807cf8fba5f1e4d
Stark ser <115328903+ysstarkx@users.noreply.github.com>
2023-10-16 13:42:16 +0530

    Create Intex.js

---
+++ Intex.js
@@ -0,0 +1,21 @@
+const client = require('./lib/client')
+
+
+
+const connect = async () => {
+
+	try {
+
+		await client.connect()
+
+	} catch (error) {
+
+		console.error(error)
+
+	}
+
+}
+
+
+
+connect()
